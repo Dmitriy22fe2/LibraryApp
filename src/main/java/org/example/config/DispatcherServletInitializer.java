384d6c00a363ec4a23c6d33922f172bff6974d98
UserD <UserD@email.com>
2025-08-20 22:38:11 +0300

    default setup

---
+++ src/main/java/org/example/config/DispatcherServletInitializer.java
@@ -0,0 +1,38 @@
+package org.example.config;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import org.jspecify.annotations.Nullable;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+
+
+public class DispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?> @Nullable [] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?> @Nullable [] getServletConfigClasses() {
+        return new Class[] {SpringConfiguration.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+
+
+
+    @Override
+    public void onStartup(ServletContext aServletContext) throws ServletException {
+        super.onStartup(aServletContext);
+        registerHiddenFieldFilter(aServletContext);
+    }
+
+    public void registerHiddenFieldFilter(ServletContext aServletContext) {
+        aServletContext.addFilter("hiddenHttpMethodFilter", new HiddenHttpMethodFilter()).addMappingForUrlPatterns(null, true, "/*");
+    }
+}
